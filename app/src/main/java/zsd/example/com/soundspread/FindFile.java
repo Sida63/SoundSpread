b22173459176e31f61ac782f58c2a3d8f4937710
ikoez <siyuansiyuan.chen@mail.utoronto.ca>
2016-03-07 18:35:28 -0800

    scan mp3 file in different file

--- app/src/main/java/zsd/example/com/soundspread/FindFile.java
+++ app/src/main/java/zsd/example/com/soundspread/FindFile.java
@@ -18,6 +18,7 @@ import java.util.regex.Pattern;
 
 public class FindFile extends AppCompatActivity {
     private String[] items;
+    private String[] itempath;
     private ArrayList<HashMap<String, Object>> listItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +43,8 @@ public class FindFile extends AppCompatActivity {
                 Intent intent = new Intent();
                 intent.setClass(FindFile.this, MainActivity.class);
                 Bundle bundle = new Bundle();
-                bundle.putSerializable("uri", "/mnt/sdcard/soundspread/" + items[arg2]);
+                bundle.putSerializable("uri", itempath[arg2]);
+                Toast.makeText(FindFile.this, itempath[arg2], Toast.LENGTH_SHORT);
                 intent.putExtras(bundle);
                 startActivity(intent);
                 finish();
@@ -53,27 +55,56 @@ public class FindFile extends AppCompatActivity {
 
     public void scanfolder(){
         String path="/mnt/sdcard/soundspread/";
-        Toast.makeText(FindFile.this, path, Toast.LENGTH_SHORT);
+       // Toast.makeText(FindFile.this, path, Toast.LENGTH_SHORT);
         File file=new File(path);
         File[] filelist =file.listFiles();
         List<String> filename=new ArrayList<String>();
+        List<String> filepath=new ArrayList<String>();
         /*listItem = new ArrayList<HashMap<String, Object>>();
         for(int i=0;i<filelist.length;i++){
             HashMap<String, Object> map = new HashMap<String, Object>();
             map.put("filename",filelist[i].getName().toString());
             listItem.add(map);*/
         for(int i=0;i<filelist.length;i++){
+           // filepath.add(path+filelist[i].getName().toString());
             Pattern p = Pattern.compile(".+\\..+");
             Matcher m=p.matcher(filelist[i].getName().toString());
            if(m.find()==true)
             {
+
+                filepath.add(path+filelist[i].getName().toString());
                 filename.add(filelist[i].getName().toString());
             }
 
         }
+
+
+        String path2="/mnt/sdcard/Download/";
+        // Toast.makeText(FindFile.this, path, Toast.LENGTH_SHORT);
+        File file2=new File(path2);
+        File[] filelist2 =file2.listFiles();
+
+
+        for(int i=0;i<filelist2.length;i++){
+            // filepath.add(path+filelist[i].getName().toString());
+            Pattern p = Pattern.compile(".+\\..+");
+            String namestring=filelist2[i].getName().toString();
+            Matcher m=p.matcher(namestring);
+            if((m.find()==true)&(namestring.endsWith(".mp3")))
+            {
+
+                filepath.add(path2+namestring);
+                filename.add(namestring);
+            }
+
+        }
+
         items=new String[filename.size()];
         for(int i=0;i<filename.size();i++)
         items[i]=filename.get(i).toString();
+        itempath=new String[filepath.size()];
+        for(int i=0;i<filepath.size();i++)
+            itempath[i]=filepath.get(i).toString();
 
     }
 
